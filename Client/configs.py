16ab2e6db066e44b516cd199432520c9d60620b4
Tristan Protzman <tlprotzman+github@gmail.com>
2017-04-29 12:26:52 -0400

    Fix port number on client configs

--- Client/configs.py
+++ Client/configs.py
@@ -1,2 +1,2 @@
-server = {"ip" : '', "port" : 4284}			# Ip is a string, port is an int
+server = {"ip" : '', "port" : 4285}			# Ip is a string, port is an int
 login = {"user" : '', "token" : ''}			# user is your username, token is your password
